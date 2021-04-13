5d7e525b64a330be69ca824e4ed10bc5ff21e013
Shanessa Kostaman <i450080@fhict.nl>
2021-04-14 00:32:14 +0200

    profileAndEmployeeList

--- prjTest/classes/user.Class.php
+++ prjTest/classes/user.Class.php
@@ -43,5 +43,68 @@ class user extends dbConnection
               }
         }
     }
+    public function RetrieveEmployeeInfo()
+    {
+        if(isset($_SESSION['sess_user_id']))
+        {
+            try
+            {
+                $id = $_SESSION['sess_user_id'];
+                $query = "SELECT * from `addingemployees` where Employee_ID = $id limit 1";
+                $stmt = $this -> connect() -> prepare($query);
+                $stmt -> execute();
+                $result = $stmt -> fetch(PDO::FETCH_ASSOC);
+                $_SESSION['sess_user_id'] = $result['Employee_ID'];
+                $_SESSION['sess_first_name'] = $result['FirstName'];
+                $_SESSION['sess_last_name'] = $result['LastName'];
+                $_SESSION['sess_date_of_birth'] = $result['DateOfBirth'];
+                $_SESSION['sess_BSN'] = $result['BSN'];
+                $_SESSION['sess_gender'] = $result['Gender'];
+                $_SESSION['sess_relationship'] = $result['Relationship'];
+                $_SESSION['sess_email'] = $result['Email'];
+                $_SESSION['sess_phone_number'] = $result['PhoneNumber'];
+                $_SESSION['sess_address'] = $result['Address'];
+                $_SESSION['sess_postal_code'] = $result['PostalCOde'];
+                $_SESSION['sess_city'] = $result['City'];
+                $_SESSION['sess_country'] = $result['Country'];
+
+            }
+            catch (PDOException $e) 
+            {
+                echo "Error : ".$e->getMessage();
+            }
+           
+        }
+        header('location:viewProfile.php');
+    }
+}
+public function UpdateInformation()
+{
+    if(isset($_POST['updateBtn']))
+    {
+        $firstName = $_POST['first_name'];
+        $lastName = $_POST['last_name'];
+        $dob = $_POST['date_of_birth'];
+        $bsn = $_POST['bsn'];
+        $gender = $_POST['gender'];
+        $relationship = $_POST['relationship'];
+        $email = $_POST['email'];
+        $pNumber = $_POST['phone_number'];
+        $address = $_POST['address'];
+        $pCode = $_POST['postal_code'];
+        $city = $_POST['city'];
+        $country = $_POST['country'];
+        try
+        {
+            $query = "UPDATE `addingemployees` SET `FirstName`= ? ,`LastName`= ?,`DateOfBirth`= ? ,`BSN`= ?,`Gender`= ?,`Relationship`= ?,`Email`= ?,`PhoneNumber`= ?,`Address`= ?,`PostalCode`= ?,`City`= ?,`Country`= ? WHERE `Employee_ID` = $_SESSION[sess_user_id]";
+            $stmt = $this -> connect() -> prepare($query);
+            $stmt -> execute([$firstName,$lastName, $dob, $bsn, $gender, $relationship, $email, $pNumber, $address, $pCode, $city, $country]);
+            header('location:retrieveInfo.php');  
+        }
+        catch (PDOException $e)
+        {
+            echo "Error : ".$e->getMessage();
+        }
+    } 
 }
 ?>
